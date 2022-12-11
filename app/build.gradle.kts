3d95ed3ff1334e46f2f2b9213fdfa43004aeb4d3
SunYoung <baby_ash@naver.com>
2022-12-11 20:41:36 +0900

    tablayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         jvmTarget = "1.8"
     }
 
-    buildFeatures{
+    buildFeatures {
         dataBinding = true
         viewBinding = true
         compose = true
@@ -39,15 +39,15 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.compose.ui:ui:1.3.1")
+    implementation("androidx.compose.ui:ui:1.3.2")
     implementation("androidx.compose.material:material:1.3.1")
     implementation("androidx.compose.material:material-icons-extended:1.3.1")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
     implementation("androidx.fragment:fragment-ktx:1.5.4")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.2")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -58,13 +58,12 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
 
     implementation("io.coil-kt:coil-compose:2.2.2")
-    implementation("androidx.fragment:fragment-ktx:1.5.4")
 
     implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:11.1.0")
-    implementation("androidx.compose.ui:ui-viewbinding:1.3.1")
+    implementation("androidx.compose.ui:ui-viewbinding:1.3.2")
     implementation("androidx.compose.material3:material3:1.1.0-alpha02")
 
-    implementation("androidx.compose.runtime:runtime-livedata:1.3.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.3.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 
     implementation("androidx.room:room-ktx:2.4.3")
--- app/src/main/java/com/cinema/classic/ui/article/PostContent.kt
+++ app/src/main/java/com/cinema/classic/ui/article/PostContent.kt
@@ -2,19 +2,20 @@ package com.cinema.classic.ui.article
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,6 +23,7 @@ import androidx.compose.ui.viewinterop.AndroidViewBinding
 import com.cinema.classic.data.interest.MovieClip
 import com.cinema.classic.databinding.FragmentContainerYoutubeBinding
 import com.cinema.classic.fragment.YoutubeFragment
+import com.cinema.classic.model.NaverMovie
 import com.cinema.classic.ui.theme.ClassicTheme
 import com.cinema.classic.ui.theme.ClassicTheme2
 import com.cinema.classic.ui.utils.BookmarkButton
@@ -41,11 +43,11 @@ private val defaultSpacerSize = 16.dp
 fun movieData(video_id: String, viewModel: VideoViewModel) {
     val data1 by viewModel.data.observeAsState()
     val data2 by viewModel.movieClipList.observeAsState()
-
     ClassicTheme2 {
         data1?.let {
-            LazyColumn {
-                item {
+            Column {
+                Column(modifier = Modifier
+                    .height(IntrinsicSize.Min)) {
                     if (!LocalInspectionMode.current) {
                         if (data2?.size == 0) {
                             YoutubePlayerView(video_id, 0f)
@@ -53,36 +55,49 @@ fun movieData(video_id: String, viewModel: VideoViewModel) {
                             YoutubePlayerView(video_id, data2?.get(0)?.time!!)
                         }
                     }
-                    Spacer(Modifier.height(defaultSpacerSize))
-                    Text(
-                        it.title.replace("<b>", "").replace("</b>", "") + ", " + it.pubDate,
-                        style = MaterialTheme.typography.h4,
-                        modifier = Modifier.padding(start = 10.dp)
-                    )
-                    Spacer(Modifier.height(8.dp))
-                    if (it.director != "") {
-                        val directors =
-                            it.director.replace("|", ",").substring(0, it.director.length - 1)
-                        Text(
-                            directors,
-                            style = MaterialTheme.typography.body1,
-                            modifier = Modifier.padding(start = 10.dp)
-                        )
-                    }
-                    if (it.actor != "") {
-                        val actors = it.actor.replace("|", ",").substring(0, it.actor.length - 1)
-                        Text(
-                            actors,
-                            style = MaterialTheme.typography.body2,
-                            modifier = Modifier.padding(start = 10.dp)
-                        )
-                    }
-                    Spacer(Modifier.height(defaultSpacerSize))
-                    tabLayout(viewModel)
+                }
+                Column(modifier = Modifier
+                    .height(IntrinsicSize.Min)
+                    .fillMaxWidth()) {
+                    detail(data1!!)
+                }
+                Column(modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()) {
+                    tabLayout(viewModel = viewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+fun detail(it: NaverMovie) {
+    Spacer(Modifier.height(defaultSpacerSize))
+    Text(
+        it.title.replace("<b>", "").replace("</b>", "") + ", " + it.pubDate,
+        style = MaterialTheme.typography.h4,
+        modifier = Modifier.padding(start = 10.dp)
+    )
+    Spacer(Modifier.height(8.dp))
+    if (it.director != "") {
+        val directors =
+            it.director.replace("|", ",").substring(0, it.director.length - 1)
+        Text(
+            directors,
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier.padding(start = 10.dp)
+        )
+    }
+    if (it.actor != "") {
+        val actors = it.actor.replace("|", ",").substring(0, it.actor.length - 1)
+        Text(
+            actors,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.padding(start = 10.dp)
+        )
+    }
+    Spacer(Modifier.height(defaultSpacerSize))
 }
 
 @Composable
@@ -101,66 +116,68 @@ private fun tabLayout(viewModel: VideoViewModel) {
     var list = viewModel.movieClipList.observeAsState()
     val plot by viewModel.plotData.observeAsState()
 
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-        val pagerState = rememberPagerState()
-        val coroutineScope = rememberCoroutineScope()
-        val scrollState = rememberScrollState()
-
-        TabRow(
-            selectedTabIndex = pagerState.currentPage,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
-                )
-            }
-        ) {
-            pages.forEachIndexed { index, title ->
-                Tab(
-                    text = { Text(title) },
-                    selected = pagerState.currentPage == index,
-                    onClick = {
-                        coroutineScope.launch {
-                            pagerState.scrollToPage(index)
-                        }
-                    },
-                    modifier = Modifier.fillMaxHeight()
-                )
-            }
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+            )
         }
-        HorizontalPager(
-            count = pages.size,
-            state = pagerState /* TO DO height programmcally*/,
-            modifier = Modifier.height(300.dp)
-        ) { page ->
-            if (page == 0) {
-                Column(modifier = Modifier
-                    .fillMaxHeight()
-                    .verticalScroll(scrollState)) {
-                    plot?.plotText?.let {
-                        Text(
-                            modifier = Modifier
-                                .wrapContentSize()
-                                .padding(10.dp),
-                            text = it,
-                            style = MaterialTheme.typography.body2
-                        )
+    ) {
+        pages.forEachIndexed { index, title ->
+            Tab(
+                text = { Text(title) },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    coroutineScope.launch {
+                        pagerState.scrollToPage(index)
                     }
+                },
+                modifier = Modifier.height(IntrinsicSize.Min)
+            )
+        }
+    }
+
+    HorizontalPager(
+        count = pages.size,
+        state = pagerState /* TO DO height programmcally*/,
+        modifier = Modifier.fillMaxSize()
+    ) { page ->
+        if (page == 0) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+            ) {
+                plot?.plotText?.let {
+                    Text(
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .padding(10.dp),
+                        text = it,
+                        style = MaterialTheme.typography.body2
+                    )
                 }
-            } else {
-                LazyColumn(modifier = Modifier.fillMaxHeight()) {
-                    list.value?.let {
-                        items(list.value!!) { post ->
-                            PostItem(post = post, onToggleFavorite = {
-                                coroutineScope.launch {
-                                    viewModel.removeMovieClip(post)
-                                }
-                            })
-                        }
+            }
+        } else {
+            LazyColumn(modifier = Modifier.fillMaxHeight()) {
+                list.value?.let {
+                    items(list.value!!) { post ->
+                        PostItem(post = post, onToggleFavorite = {
+                            coroutineScope.launch {
+                                viewModel.removeMovieClip(post)
+                            }
+                        })
                     }
                 }
             }
         }
     }
+
+
 }
 
 @SuppressLint("SimpleDateFormat")
@@ -210,6 +227,26 @@ fun PreviewPost() {
     ClassicTheme {
         Surface {
             PostItem(post = MovieClip("2j7uys48wwc", 2266.77F, Calendar.getInstance()), {})
+        }
+    }
+}
+
+@Preview
+@Composable
+fun content() {
+    Row(
+        modifier = Modifier
+            .height(IntrinsicSize.Min)
+    ) {
+        Box(
+            modifier = Modifier
+                .width(8.dp)
+                .fillMaxHeight()
+                .background(Color.Red)
+        )
+        Column {
+            Text("Hello")
+            Text("World")
         }
     }
 }
